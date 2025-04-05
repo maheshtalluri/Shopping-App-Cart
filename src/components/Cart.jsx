3334aa2bcaaeab6a9f30b183bf228d64a120598d
Mahesh Kumar Talluri <tallurimahesh169@gmail.com>
2025-04-05 13:18:54 +0530

    updated-3

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React from "react";
 import CartItem from "./CartItem";
 import ProgressBar from "./ProgressBar";
@@ -38,44 +38,3 @@ const Cart = ({ cart, updateQuantity, removeItem, subtotal }) => {
 };
 
 export default Cart;
-=======
-import React from "react";
-import CartItem from "./CartItem";
-import ProgressBar from "./ProgressBar";
-import { THRESHOLD } from "../constants";
-
-const Cart = ({ cart, updateQuantity, removeItem, subtotal }) => {
-  return (
-    <div>
-        <h2>Cart Summary:</h2>
-        <div className="cart">
-      <div className = "subtotal" >
-        <h3>Subtotal:</h3>
-        <h3> ₹{subtotal}</h3>
-      </div>
-      <hr/>
-      
-      <ProgressBar value={subtotal} threshold={THRESHOLD} />
-      <div className="cart-items">
-        {cart.length === 0 ? "" :<h3>Cart Items</h3> }
-      
-        {cart.length === 0 ? (
-            <div className = "empty" >
-                <h5>Your cart is empty</h5>
-                <p>Add some products to see them here!</p>
-            </div>
-          
-        ) : (
-          cart.map((item) => (
-            <CartItem key={item.id} item={item} updateQuantity={updateQuantity} removeItem={removeItem} />
-          ))
-        )}
-      </div>
-    </div>
-    </div>
-    
-  );
-};
-
-export default Cart;
->>>>>>> 05f1d3aaa41f1188034041fedf0345b92658c662
--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React from "react";
 
 const ProgressBar = ({ value, threshold }) => {
@@ -22,28 +22,3 @@ const ProgressBar = ({ value, threshold }) => {
 };
 
 export default ProgressBar;
-=======
-import React from "react";
-
-const ProgressBar = ({ value, threshold }) => {
-  const progress = Math.min((value / threshold) * 100, 100);
-
-  return (
-    <div className="progress-bar">
-      {value >= threshold ? (
-        <div className="free-gift">
-          <p>You got a free Wireless Mouse!</p>
-          <p className="get">Get Free Gift</p>
-        </div>
-      ) : (
-        `Add ₹${threshold - value} more to get a free gift!`
-      )}
-      <div className="bar">
-        <div className="fill" style={{ width: `${progress}%` }}></div>
-      </div>
-    </div>
-  );
-};
-
-export default ProgressBar;
->>>>>>> 05f1d3aaa41f1188034041fedf0345b92658c662
